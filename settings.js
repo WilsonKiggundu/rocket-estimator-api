768459a616d23be1676c567214162752d83a8a47
wilson <wilson@laboremus.no>
2019-05-27 16:43:41 +0300

    Changes test database

--- settings.js
+++ settings.js
@@ -4,5 +4,5 @@ export default {
   database:
     process.env.ENV === 'Test'
       ? 'mongodb://localhost/estimator_test'
-      : 'mongodb://localhost/estimator',
+      : 'mongodb://ekastimo:Oe6aGK02xsqoHlVN@cluster0-shard-00-00-shw15.mongodb.net:27017,cluster0-shard-00-01-shw15.mongodb.net:27017,cluster0-shard-00-02-shw15.mongodb.net:27017/rocket_estimator_database?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
 };
